2c0b4dd2bb2d248731cbffa0bcdcb6a327712e2e
Pratyaksh Gupta <150325858+Pratyakshgupta887qwert@users.noreply.github.com>
2026-03-02 11:48:46 +0530

    Refactor authDatabase.js for DATABASE_URL and SSL
    Updated Sequelize configuration to use DATABASE_URL and added SSL options.

--- backend/src/config/authDatabase.js
+++ backend/src/config/authDatabase.js
@@ -1,18 +1,34 @@
 import { Sequelize } from "sequelize";
-import dotenv from "dotenv";
 
-dotenv.config();
-
-const authSequelize = new Sequelize(
-  process.env.AUTH_DB_NAME,
-  process.env.AUTH_DB_USER,
-  process.env.AUTH_DB_PASSWORD,
-  {
-    host: process.env.AUTH_DB_HOST,
-    port: process.env.AUTH_DB_PORT,
-    dialect: "postgres",
-    logging: false,
+const authSequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: "postgres",
+  protocol: "postgres",
+  logging: false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
   },
-);
+});
 
 export default authSequelize;
+
+// import { Sequelize } from "sequelize";
+// // import dotenv from "dotenv";
+
+// // dotenv.config();
+
+// const authSequelize = new Sequelize(
+//   process.env.AUTH_DB_NAME,
+//   process.env.AUTH_DB_USER,
+//   process.env.AUTH_DB_PASSWORD,
+//   {
+//     host: process.env.AUTH_DB_HOST,
+//     port: process.env.AUTH_DB_PORT,
+//     dialect: "postgres",
+//     logging: false,
+//   },
+// );
+
+// export default authSequelize;
