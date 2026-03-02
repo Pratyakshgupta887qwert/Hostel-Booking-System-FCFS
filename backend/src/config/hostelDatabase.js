34f48a0614bf8fdb60bd81570887c9a70cfded05
Pratyaksh Gupta <150325858+Pratyakshgupta887qwert@users.noreply.github.com>
2026-03-02 11:50:04 +0530

    Refactor Sequelize connection for database access
    Updated Sequelize configuration to use DATABASE_URL and added SSL options.

--- backend/src/config/hostelDatabase.js
+++ backend/src/config/hostelDatabase.js
@@ -1,18 +1,34 @@
 import { Sequelize } from "sequelize";
-import dotenv from "dotenv";
 
-dotenv.config();
-
-const hostelSequelize = new Sequelize(
-  "hostel_db",
-  process.env.AUTH_DB_USER,
-  process.env.AUTH_DB_PASSWORD,
-  {
-    host: process.env.AUTH_DB_HOST,
-    port: process.env.AUTH_DB_PORT,
-    dialect: "postgres",
-    logging: false,
+const hostelSequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: "postgres",
+  protocol: "postgres",
+  logging: false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
   },
-);
+});
 
 export default hostelSequelize;
+
+// import { Sequelize } from "sequelize";
+// import dotenv from "dotenv";
+
+// dotenv.config();
+
+// const hostelSequelize = new Sequelize(
+//   "hostel_db",
+//   process.env.AUTH_DB_USER,
+//   process.env.AUTH_DB_PASSWORD,
+//   {
+//     host: process.env.AUTH_DB_HOST,
+//     port: process.env.AUTH_DB_PORT,
+//     dialect: "postgres",
+//     logging: false,
+//   },
+// );
+
+// export default hostelSequelize;
